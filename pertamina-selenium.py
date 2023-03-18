1e61e4e6cdc6b94ad86a942f976b3215ab74cacb
Febriyant Abidin <dtmp23167@C02H20Z8Q05P.local>
2023-03-18 22:50:30 +0700

    success to scrape

--- pertamina-selenium.py
+++ pertamina-selenium.py
@@ -10,13 +10,76 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+def bbmtype(var1):
+    match var1:
+        case 0:
+            return "Pertamax Turbo"
+        case 1:
+            return "Pertamax"
+        case 2:
+            return "Pertalite"
+        case 3:
+            return "Pertamina Dex"
+        case 4:
+            return "Pertamina Dexlite"
+        case 5:
+            return "Bio Solar"
+        case default:
+            return "none"
+
 driver = webdriver.Chrome()
 driver.get("https://mypertamina.id/fuels-harga")
 pertaminashoup = driver.page_source
 pshoup = BeautifulSoup(pertaminashoup,'lxml')
-fdiv = pshoup.find_all("div",{"class": "card-body"})
 
-print(fdiv)
+
+
+nvalue = range(0,6)
+for nll in nvalue:
+    lsprov=[]
+    lsharga=[]
+    d ={}
+    bbmtipe = bbmtype(nll)
+    fdiv = str(pshoup.find_all("div",{"data-slick-index": nll}))
+    getlabel = BeautifulSoup(fdiv,'lxml')
+    provinsi = getlabel.find_all("label",{"class":"text-sm"})
+    harga = getlabel.find_all("label",{"class":"text-danger"})
+
+    # Make List Provinsi
+    for lprov in provinsi:
+        lsprov.append(lprov.text)
+    
+    for lharga in harga:
+        lsharga.append(lharga.text)
+    
+    # remove prefix Prov.
+    clean1 = [i.replace('Prov. ', '') for i in lsprov]
+    # remove prefix Rp.
+    clean2 = [h.replace('Rp ', '') for h in lsharga]
+
+    # remove prefix comma separator ,
+    clean3 = [c.replace(',', '') for c in clean2]
+
+    # remove prefix -
+    clean4 = [c3.replace('-', '0') for c3 in clean3]
+
+    d = {'tipebbm':bbmtipe, 'provinsi':clean1, 'harga':clean4}
+    pd.set_option('display.max_rows', None)
+    df = pd.DataFrame(d)
+    print(df)
+
+# # Make Dict
+# mpertamina = dict(zip(clean1, clean3))
+# print(mpertamina)
+
+
+# Make Data Frame
+
+
+# print(lsprov)
+# print(pshoup.find_all("img",{"class":"img-fluid"}))
+# print(help(fdiv))
+
 driver.implicitly_wait(60) # 10 second wait
 # bbmprice = driver.find_element(By.CLASS_NAME,"card")
 # jsjsj = bbmprice.find_element(By.CLASS_NAME,"card-body")
